3e52e1423e0bb35b4d4ad7694dbcdf9c2a1c730a
oseni ade <oseniadedoyin22@gmail.com>
2023-06-14 01:18:42 +0100

    change name

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -3,7 +3,7 @@ import Graph from "./Graph";
 const Header = () => {
   return (
     <div className="text-center flex justify-center gap-4 flex-col items-center py-10">
-      <h1 className="text-[16px]  font-extrabold">Counter App</h1>
+      <h1 className="text-[16px]  font-extrabold">RLW Usher's Counter</h1>
       <div className="">
         <Graph />
       </div>
